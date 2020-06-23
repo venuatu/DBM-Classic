1561abdb8bf8271ed7110cb4810ded6eb3fd9187
MysticalOS <mysticalosx@gmail.com>
2020-06-22 21:15:25 -0400

    Removed syncing from molten core and BWL raids, as of combat log hotfix Changed way eggs are calculated on razorgore to see how it works with extended combat log range

--- DBM-MC/Shazzrah.lua
+++ DBM-MC/Shazzrah.lua
@@ -73,21 +73,9 @@ do
 			timerCounterSpellCD:Start()
 		--elseif args.spellId == 23138 then
 		elseif spellName == Gate then
-			self:SendSync("Teleport", args.sourceName)
-			if self:AntiSpam(5, 1) then
-				specWarnGate:Show(args.sourceName)
-				specWarnGate:Play("tauntboss")
-				timerGateCD:Start()
-			end
+			specWarnGate:Show(args.sourceName)
+			specWarnGate:Play("tauntboss")
+			timerGateCD:Start()
 		end
-	end
-end
-
-function mod:OnSync(msg, targetName)
-	if not self:IsInCombat() then return end
-	if msg == "Teleport" and targetName and self:AntiSpam(5, 1) then
-		specWarnGate:Show(targetName)
-		specWarnGate:Play("tauntboss")
-		timerGateCD:Start()
 	end
 end
