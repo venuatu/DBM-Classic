c0b7cfd8174c24975bc5ba496245565298671283
anon1231823 <67269448+anon1231823@users.noreply.github.com>
2020-07-16 13:17:53 +0000

    Update localization.de.lua (#343)
    Co-authored-by: Adam <MysticalOS@users.noreply.github.com>

--- DBM-BWL/localization.de.lua
+++ DBM-BWL/localization.de.lua
@@ -17,9 +17,9 @@ L:SetOptionLocalization{
 }
 L:SetMiscLocalization{
 	Phase2Emote	= "flieht während die kontrollierenden Kräfte der Kugel schwinden.",
-  YellEgg1 = "Ihr werdet dafür bezahlen, dass Ihr mich zu soetwas gezwungen habt!",
-  YellEgg2 = "Narren! Diese Eier sind weit wertvoller als Ihr glaubt!",
-  YellEgg3 = "Nein - nicht noch einer! Für diese Gräueltat werdet Ihr mit Euren Köpfen bezahlen!",
+	YellEgg1 = "Ihr werdet dafür bezahlen, dass Ihr mich zu soetwas gezwungen habt!",
+	YellEgg2 = "Narren! Diese Eier sind weit wertvoller als Ihr glaubt!",
+	YellEgg3 = "Nein - nicht noch einer! Für diese Gräueltat werdet Ihr mit Euren Köpfen bezahlen!",
 	YellPull 	= "Eindringlinge sind in die Brutstätte vorgestoßen! Schlagt Alarm! Beschützt die Eier um jeden Preis!"
 }
 -------------------
@@ -74,6 +74,29 @@ L:SetGeneralLocalization{
 	name = "Flammenmaul"
 }
 
+-----------------------
+--  Vulnerabilities  --
+-----------------------
+-- Chromaggus, Death Talon Overseer and Death Talon Wyrmguard
+L = DBM:GetModLocalization("TalonGuards")
+
+L:SetGeneralLocalization{
+	name = "Todeskrallenwache"
+}
+L:SetWarningLocalization{
+	WarnVulnerable = "%sanfälligkeit"
+}
+L:SetOptionLocalization{
+	WarnVulnerable = "Zeige Warnung für Zauberanfälligkeiten"
+}
+L:SetMiscLocalization{
+	Fire = "Feuer",
+	Nature = "Natur",
+	Frost = "Frost",
+	Shadow = "Schatten",
+	Arcane = "Arkan",
+	Holy = "Heilig"
+}
 ------------------
 --  Chromaggus  --
 ------------------
@@ -83,18 +106,32 @@ L:SetGeneralLocalization{
 	name = "Chromaggus"
 }
 L:SetWarningLocalization{
-	WarnBreath		= "%s"
+	WarnBreath = "%s",
+	WarnVulnerable = "%sanfälligkeit"
 }
 L:SetTimerLocalization{
-	TimerBreathCD	= "%s CD"
+	TimerBreathCD = "Abklingzeit der %s",
+	TimerBreath = "%s Zauber",
+	TimerVulnCD = "Abklingzeit der Anfälligkeit"
 }
 L:SetOptionLocalization{
-	WarnBreath		= "Zeige Warnung, wenn Chromaggus einen seiner Atem wirkt",
-	TimerBreathCD	= "Abklingzeit der Atem anzeigen"
+	WarnBreath = "Zeige Warnung, wenn Chromaggus einen seiner Atem wirkt",
+	WarnVulnerable = "Zeige Warnung für Zauberanfälligkeiten",
+	TimerBreathCD = "Abklingzeit des Atem anzeigen",
+	TimerBreath = "Zeige Atem Zauber",
+	TimerVulnCD = "Zeige Abklingzeit der Anfälligkeit"
 }
 L:SetMiscLocalization{
-	Breath1	= "Erster Atem",
-	Breath2	= "Zweiter Atem"
+	Breath1 = "Erster Atem",
+	Breath2 = "Zweiter Atem",
+	VulnEmote = "%s weicht zurück, als die Haut schimmert.",
+	Vuln = "Anfälligkeit",
+	Fire = "Feuer",
+	Nature = "Natur",
+	Frost = "Frost",
+	Shadow = "Schatten",
+	Arcane = "Arkan",
+	Holy = "Heilig"
 }
 
 ----------------
@@ -106,16 +143,21 @@ L:SetGeneralLocalization{
 	name = "Nefarian"
 }
 L:SetWarningLocalization{
-	WarnClassCall		= "%s Ruf"
+	WarnAddsLeft = "%d ausstehende Tötungen",
+	WarnClassCall = "%s Ruf",
+	specwarnClassCall = "Klassenruf auf Dir!"
 }
 L:SetTimerLocalization{
-	TimerClassCall		= "%s Ruf endet"
+	TimerClassCall = "%s Ruf endet"
 }
 L:SetOptionLocalization{
-	TimerClassCall		= "Dauer der Klassenrufe anzeigen",
-	WarnClassCall		= "Verkünde Klassenrufe"
+	TimerClassCall = "Dauer der Klassenrufe anzeigen",
+	WarnAddsLeft = "Kündige verbleibend Tötungen an bis 2 Phase ausgelöst ist.",
+	WarnClassCall = "Verkünde Klassenrufe",
+	specwarnClassCall = "Zeige besondere Warnung wenn Du von einem Klassenruf betroffen bist"
 }
 L:SetMiscLocalization{
+	YellP1		= "Let the games begin!",
 	YellP2		= "Sehr gut, meine Diener. Der Mut der Sterblichen scheint zu schwinden! Nun lasst uns sehen, wie sie sich gegen den wahren Herrscher des Schwarzfels behaupten werden!",
 	YellP3		= "Unmöglich! Erhebt Euch, meine Diener! Kämpft erneut für Euren Meister!",
 	YellShaman	= "Schamane, zeigt mir was eure Totems können!",
