a20f298329e6ee702b7ee827c01aeffb10646069
枫聖 <2303601+nanjuekaien1@users.noreply.github.com>
2020-08-17 23:47:23 +0800

    Update localization.cn.lua (#470)

--- DBM-AQ40/localization.cn.lua
+++ DBM-AQ40/localization.cn.lua
@@ -128,8 +128,9 @@ L:SetOptionLocalization{
 	RangeFrame				= "显示距离框架(10码)"
 }
 L:SetMiscLocalization{
-	Stomach		= "克图恩的肚子",
+	Stomach		= "克图恩的胃",
 	Eye			= "克苏恩之眼",
+	FleshTent	= "血肉触须",--Localized so it shows on frame in users language, not senders
 	Weakened 	= "削弱了",
     NotValid	= "AQ40 击杀信息： %s 首领未击杀。"
 }
