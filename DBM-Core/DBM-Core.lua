457300437d7715dfcaf8c4340e1c3f1e7ab3f1b6
MysticalOS <mysticalosx@gmail.com>
2020-08-26 18:35:27 -0400

    end

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -2754,7 +2754,7 @@ do
 --				DBM:AddMsg(L.SILENTMODE_IS .. (DBM.Options.SilentMode and "ON" or "OFF"))
 --			else
 				DBM:LoadGUI()
-			end
+--			end
 		end
 
 		function dataBroker.OnTooltipShow(GameTooltip)
