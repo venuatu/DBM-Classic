725bc8a20fc6babe6825b6ee8301396ecdd63353
MysticalOS <mysticalosx@gmail.com>
2020-12-07 17:34:30 -0500

    Tag last classic release for a while. There will likely be no further updates until the new year, do to launch of castle nathria tomorrow.

--- DBM-Core/DBM-Core.lua
+++ DBM-Core/DBM-Core.lua
@@ -72,8 +72,8 @@ end
 
 DBM = {
 	Revision = parseCurseDate("@project-date-integer@"),
-	DisplayVersion = "1.13.63 alpha", -- the string that is shown as version
-	ReleaseRevision = releaseDate(2020, 12, 1) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
+	DisplayVersion = "1.13.63", -- the string that is shown as version
+	ReleaseRevision = releaseDate(2020, 12, 7) -- the date of the latest stable version that is available, optionally pass hours, minutes, and seconds for multiple releases in one day
 }
 DBM.HighestRelease = DBM.ReleaseRevision --Updated if newer version is detected, used by update nags to reflect critical fixes user is missing on boss pulls
 
