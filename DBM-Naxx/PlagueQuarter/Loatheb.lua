8c322823a419904b9f02237b71ee59ccbc1c9a3a
MysticalOS <mysticalosx@gmail.com>
2020-12-25 19:13:39 -0500

    Added support for "Remove Curse". Closes #651

--- DBM-Naxx/PlagueQuarter/Loatheb.lua
+++ DBM-Naxx/PlagueQuarter/Loatheb.lua
@@ -8,20 +8,22 @@ mod:SetModelID(16110)
 mod:RegisterCombat("combat")--Maybe change to a yell later so pull detection works if you chain pull him from tash gauntlet
 
 mod:RegisterEventsInCombat(
-	"SPELL_CAST_SUCCESS 29234 29204",
+	"SPELL_CAST_SUCCESS 29234 29204 30281",
 	"UNIT_DIED"
 )
 
 --TODO, the 5xxxx spellIds are not from classic
-local warnSporeNow	= mod:NewCountAnnounce(29234, 2, "134530")
-local warnSporeSoon	= mod:NewSoonAnnounce(29234, 1, "134530")
-local warnDoomNow	= mod:NewSpellAnnounce(29204, 3)
---local warnHealSoon	= mod:NewAnnounce("WarningHealSoon", 4, 55593)
---local warnHealNow	= mod:NewAnnounce("WarningHealNow", 1, 55593, false)
+local warnSporeNow			= mod:NewCountAnnounce(29234, 2, "134530")
+local warnSporeSoon			= mod:NewSoonAnnounce(29234, 1, "134530")
+local warnDoomNow			= mod:NewSpellAnnounce(29204, 3)
+local warnRemoveCurse		= mod:NewSpellAnnounce(30281, 3)
+--local warnHealSoon		= mod:NewAnnounce("WarningHealSoon", 4, 55593)
+--local warnHealNow			= mod:NewAnnounce("WarningHealNow", 1, 55593, false)
 
-local timerSpore	= mod:NewNextCountTimer(12.9, 29234, nil, nil, nil, 5, "134530", DBM_CORE_L.DAMAGE_ICON)-- initial 11.3 then 12.92-12.99
-local timerDoom		= mod:NewNextTimer(29, 29204, nil, nil, nil, 2)-- initial 130 then 29.1-32.4
---local timerAura		= mod:NewBuffActiveTimer(17, 55593, nil, nil, nil, 5, nil, DBM_CORE_L.HEALER_ICON)
+local timerSpore			= mod:NewNextCountTimer(12.9, 29234, nil, nil, nil, 5, "134530", DBM_CORE_L.DAMAGE_ICON)-- initial 11.3 then 12.92-12.99
+local timerDoom				= mod:NewNextTimer(29, 29204, nil, nil, nil, 2)-- initial 130 then 29.1-32.4
+local timerRemoveCurseCD	= mod:NewNextTimer(30.8, 30281, nil, nil, nil, 5)
+--local timerAura			= mod:NewBuffActiveTimer(17, 55593, nil, nil, nil, 5, nil, DBM_CORE_L.HEALER_ICON)
 
 mod.vb.doomCounter	= 0
 mod.vb.sporeTimer	= 12.9
@@ -30,13 +32,14 @@ mod.vb.sporeCounter = 0
 function mod:OnCombatStart(delay)
 	self.vb.doomCounter = 0
 	self.vb.sporeCounter = 0
+	timerRemoveCurseCD:Start(3 - delay)
 	timerSpore:Start(11.3 - delay, 1)
 	warnSporeSoon:Schedule(self.vb.sporeTimer - 5 - delay)
 	timerDoom:Start(130 - delay, self.vb.doomCounter + 1)
 end
 
 do
-	local Spore, InevitableDoom = DBM:GetSpellInfo(29234), DBM:GetSpellInfo(29204)
+	local Spore, InevitableDoom, RemoveCurse = DBM:GetSpellInfo(29234), DBM:GetSpellInfo(29204), DBM:GetSpellInfo(30281)
 	function mod:SPELL_CAST_SUCCESS(args)
 		--if args.spellId == 29234 then
 		if args.spellName == Spore then
@@ -53,6 +56,10 @@ do
 			end
 			warnDoomNow:Show(self.vb.doomCounter)
 			timerDoom:Start(timer, self.vb.doomCounter + 1)
+		--if args.spellId == 30281 then
+		elseif args.spellName == RemoveCurse then
+			warnRemoveCurse:Show()
+			timerRemoveCurseCD:Start()
 		--elseif args.spellId == 55593 then
 			--timerAura:Start()
 			--warnHealSoon:Schedule(14)
